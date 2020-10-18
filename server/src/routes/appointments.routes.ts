6e5f754530fe57b148164ef3392d7f0d5500edc3
jennifertakagi <jennitakagi@gmail.com>
2020-10-18 16:43:04 -0300

    feat: verify if an appointment already exist

--- server/src/routes/appointments.routes.ts
+++ server/src/routes/appointments.routes.ts
@@ -1,15 +1,29 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { isEqual, startOfHour, parseISO } from 'date-fns';
+
+interface Appointment {
+  date: Date;
+  id: string;
+  provider: string;
+}
 
 const appointmentsRouter = Router();
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date))
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked 😞'})
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   }
 
   appointments.push(appointment);

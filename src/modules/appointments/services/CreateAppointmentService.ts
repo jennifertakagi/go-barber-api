e29138fc1f9bbd5071888dc49a4fb80986159468
jennifertakagi <jennitakagi@gmail.com>
2021-01-07 22:59:03 -0300

    refactor: appointment repository

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -22,12 +22,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked 😞');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }

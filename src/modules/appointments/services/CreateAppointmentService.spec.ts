c3c83d28fb513489c30aea3ddfe2c4912da56706
jennifertakagi <jennitakagi@gmail.com>
2021-01-09 17:18:26 -0300

    test: adjust date comparation and test not create two appointments on same date

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -18,7 +20,24 @@ describe('CreateAppointmentService', () => {
     expect(appointment.provider_id).toBe('123');
   });
 
-  // it('Not create two appointments on the same date', () => {
+  it('Not create two appointments on the same date', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-  // })
+    const appointmentDate = new Date(2021, 0, 9, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });

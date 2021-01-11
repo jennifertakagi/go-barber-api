e40aa10510857ca0444eee2c8fb5064a15cae38d
jennifertakagi <jennitakagi@gmail.com>
2021-01-11 19:26:33 -0300

    feat: add template mail engine

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 
@@ -31,6 +32,12 @@ class SendForgotPasswordEmailService {
     }
 
     const { token } = await this.userTokensRepository.generate(user.id);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await this.mailProvider.sendMail({
       to: {
@@ -39,10 +46,11 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Password recovery request received',
       templateData: {
-        template: 'Hello, {{name}} {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
           token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });

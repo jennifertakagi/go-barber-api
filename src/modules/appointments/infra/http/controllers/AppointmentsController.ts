bdca9a67b115357739f4eb4aee46ac6a972f5f3c
jennifertakagi <jennitakagi@gmail.com>
2021-01-16 18:27:22 -0300

    fix: cache serialized

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
@@ -16,6 +17,6 @@ export default class AppointmentsController {
       user_id,
     });
 
-    return response.json(appointment);
+    return response.json(classToClass(appointment));
   }
 }
